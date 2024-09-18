330e184a70400871e7f541f19d611584cb59478b
Andrew Izatt <izatt.andrew@gmail.com>
2024-09-17 22:09:34 -0600

    add nvim config files

---
+++ .config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("options")
+require("keymaps")
+require("commands")
+require("custom.lazy")
+
+-- if vim.fn.hostname() ~= "silvermachine" then
+--   vim.g.node_host_prog = "~/.npm-global/bin/neovim-node-host"
+-- else
+--   vim.g.node_host_prog = "/usr/local/bin/neovim-node-host"
+-- end
