f50ae73dceec8867392d199f8f514e4d6970f63f
Andrew Izatt <izatt.andrew@gmail.com>
2024-11-01 22:52:53 -0600

    refactor neovim plugins and add aliases to nvim plugin config files

--- .config/nvim/lua/custom/plugins/disabled.lua
+++ .config/nvim/lua/custom/plugins/disabled.lua
@@ -1,12 +1,59 @@
 return {
-  { "goolord/alpha-nvim", enabled = false },
-  { "rmagatti/auto-session", enabled = false },
-  { "akinsho/bufferline.nvim", enabled = false },
-  { "0xstepit/flow.nvim", enabled = false },
+  -- lsp/lspconfig.lua
+  { "neovim/nvim-lspconfig", enabled = true },
+  -- lsp/mason.lua
+  { "williamboman/mason.nvim", enabled = true },
+  -- tmux-navigator (requires plenary) (found in ~/.config/nvim/lua/custom/plugins/"
+  { "nvim-lua/plenary.nvim", enabled = true },
+
+  { "christoomey/vim-tmux-navigator", enabled = true },
+  -- alpha.lua
+  { "goolord/alpha-nvim", enabled = true },
+  -- auto-session.lua
+  { "rmagatti/auto-session", enabled = true },
+  -- bufferline.lua
+  { "akinsho/bufferline.nvim", enabled = true },
+  -- colorscheme_flow.lua
+  { "0xstepit/flow.nvim", enabled = true },
+  -- colorscheme_tokyonight.lua
   { "folke/tokyonight.nvim", enabled = false },
-  { "zbirenbaum/copilot-cmp", enabled = false },
-  { "zbirenbaum/copilot.lua", enabled = false },
-  { "CopilotC-Nvim/CopilotChat.nvim", enabled = false },
-  { "stevearc/dressing.nvim", enabled = false },
-  { "nvim-lualine/lualine.nvim", enabled = false },
+  -- copilot-cmp.lua
+  { "zbirenbaum/copilot-cmp", enabled = true },
+  -- copilot.lua
+  { "zbirenbaum/copilot.lua", enabled = true },
+  -- copilotchat.lua
+  { "CopilotC-Nvim/CopilotChat.nvim", enabled = true },
+  -- dressing.lua
+  { "stevearc/dressing.nvim", enabled = true },
+  -- formatting.lua
+  { "stevearc/conform.nvim", enabled = true },
+  -- indent-blankline.lua
+  { "lukas-reineke/indent-blankline.nvim", enabled = true },
+  -- linting.lua
+  { "mfussenegger/nvim-lint", enabled = true },
+  -- lualine.lua
+  { "nvim-lualine/lualine.nvim", enabled = true },
+  -- nvim-cmp.lua
+  { "hrsh7th/nvim-cmp", enabled = true },
+  -- nvim-tree.lua
+  -- Depends on nvim-web-devicons
+  { "nvim-tree/nvim-tree.lua", enabled = true },
+  -- substitute.lua
+  { "gbprod/substitute.nvim", enabled = true },
+  -- surround.lua
+  { "kylechui/nvim-surround", enabled = false },
+  -- telescope.lua
+  -- Depends on plenary, nvim-web-devicons, todo-comments, and trouble
+  { "nvim-telescope/telescope.nvim", enabled = true },
+  -- todo-comments.lua
+  { "folke/todo-comments.nvim", enabled = true },
+  -- treesitter.lua
+  { "nvim-treesitter/nvim-treesitter", enabled = true },
+  -- trouble.lua
+  -- Depends on nvim-web-devicons and todo-comments
+  { "folke/trouble.nvim", enabled = true },
+  -- vim-maximizer.lua
+  { "szw/vim-maximizer", enabled = true },
+  -- which-key.lua
+  { "folke/which-key.nvim", enabled = true },
 }
--- .config/nvim/lua/custom/plugins/which-key.lua
+++ .config/nvim/lua/custom/plugins/which-key.lua
@@ -5,9 +5,23 @@ return {
     vim.o.timeout = true
     vim.o.timeoutlen = 500
   end,
+  config = function(_, opts)
+    require("which-key").setup(opts)
+    -- custom highlights
+    -- This one puts a custom color besides the one inherited from the theme
+    vim.api.nvim_set_hl(0, "WhichKeyNormal", { bg = "#000000" })
+    -- vim.api.nvim_set_hl(0, "WhichKeyNormal", { link = "NormalFloat" })
+  end,
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    win = {
+      border = "single",
+      wo = {
+        winblend = 0, -- value between 0-100 0 for fully opaque and 100 for fully transparent
+      },
+    },
+    keys = {
+      scroll_down = "<c-d>", -- binding to scroll down inside the popup
+      scroll_up = "<c-u>", -- binding to scroll up inside the popup
+    },
   },
 }
