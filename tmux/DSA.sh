4e1a09aada13e9371ca4c52018aea894c2bc06ba
Andrew Izatt <izatt.andrew@gmail.com>
2024-11-27 22:00:18 -0700

    revise .tmux.conf and DSA.sh

--- tmux/DSA.sh
+++ tmux/DSA.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-tmux has-session -t DSA
-if [[ $? != 0 ]]
-then
+tmux has-session -t DSA 2>/dev/null
+if [[ $? != 0 ]]; then
     cd ~/Documents/Coding/DSA
-    tmux detach
+    # tmux detach
     tmux new-session -s DSA -n Editor -d
-    tmux split-window -v -l 5 -t DSA
+    tmux split-window -v -l 23% -t DSA
     tmux send-keys -t DSA:1.1 'nvim' C-m
 fi
 tmux attach -t DSA
