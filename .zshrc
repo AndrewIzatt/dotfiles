93df63d27eb64d122e172134deea51213e635e78
gnarwal <izatt.andrew@gmail.com>
2025-01-02 16:04:26 -0700

    update oh-my-zsh, tmux, and wezterm config

--- .zshrc
+++ .zshrc
@@ -31,6 +31,15 @@ export MYSQLSH_PROMPT_THEME=~/.mysqlsh/prompt.json
 
 alias nvim-mod='NVIM_APPNAME="kickmod" nvim'
 
+# history setup
+# HISTFILE=$HOME/.zhistory
+# SAVEHIST=1000
+# HISTSIZE=999
+# setopt share_history
+# setopt hist_expire_dups_first
+# setopt hist_ignore_dups
+# setopt hist_verify
+
 # Path to your oh-my-zsh installation.
 export ZSH="$HOME/.oh-my-zsh"
 
--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -4,7 +4,18 @@ local wezterm = require 'wezterm'
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+local act = wezterm.action
+
 -- This is where you actually apply your config choices
+
+-- Scroll by line and page
+config.keys = {
+  -- { key = 'UpArrow',   mods = 'SHIFT', action = act.ScrollByLine(-1) },
+  { key = 'k',        mods = 'SHIFT|CTRL', action = act.ScrollByLine(-1) },
+  { key = 'j',        mods = 'SHIFT|CTRL', action = act.ScrollByLine(1) },
+  { key = 'PageUp',   mods = 'SHIFT|CTRL', action = act.ScrollByPage(-1) },
+  { key = 'PageDown', mods = 'SHIFT|CTRL', action = act.ScrollByPage(1) },
+}
 
 -- For example, changing the color scheme:
 -- config.color_scheme = 'AdventureTime'
@@ -15,6 +26,11 @@ config.font = wezterm.font('RobotoMono Nerd Font Mono')
 
 config.window_decorations = "RESIZE"
 config.enable_tab_bar = false
+
+config.use_dead_keys = false
+config.scrollback_lines = 5000
+
+-- config.enable_kitty_keyboard = true
 
 -- and finally, return the configuration to wezterm
 return config
