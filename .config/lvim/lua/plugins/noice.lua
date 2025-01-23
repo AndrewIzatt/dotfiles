aff9f559092d0d762e657de5467f814dbe83be95
Andrew Izatt <izatt.andrew@gmail.com>
2025-01-22 20:06:11 -0700

    update lazyvim config

---
+++ .config/lvim/lua/plugins/noice.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "folke/noice.nvim",
+    enabled = true,
+    opts = {
+      hover = {
+        silent = true,
+      },
+      presets = {
+        lsp_doc_border = true,
+      },
+    },
+  },
+}
